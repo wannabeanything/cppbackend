f657828da3d6d5af728a1cd1c3d5ecd84e6583c5
stason <stas05728@gmail.com>
2025-06-20 07:05:28 +0000

    sent_for_review

--- sprint2/problems/static_content/solution/src/main.cpp
+++ sprint2/problems/static_content/solution/src/main.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <thread>
 #include <vector>
+#include <filesystem>
 
 #include "json_loader.h"
 #include "request_handler.h"
@@ -31,20 +32,23 @@ namespace {
 } // namespace
 
 int main(int argc, const char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: game_server <game-config-json>"sv << std::endl;
+    if (argc != 3) {
+        std::cerr << "Usage: game_server <game-config-json> <static-root>"sv << std::endl;
         return EXIT_FAILURE;
     }
 
     try {
-        // 1. Загружаем карту из файла и построить модель игры
+        // 1. Загружаем карту из файла
         model::Game game = json_loader::LoadGame(argv[1]);
 
-        // 2. Инициализируем io_context
+        // 2. Путь к статическим файлам
+        std::filesystem::path static_root = argv[2];
+
+        // 3. Инициализируем io_context
         const unsigned num_threads = std::thread::hardware_concurrency();
         net::io_context ioc(num_threads);
 
-        // 3. Обработчик сигналов
+        // 4. Обработчик сигналов
         net::signal_set signals(ioc, SIGINT, SIGTERM);
         signals.async_wait([&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number) {
             if (!ec) {
@@ -53,10 +57,10 @@ int main(int argc, const char* argv[]) {
             }
         });
 
-        // 4. HTTP обработчик
-        http_handler::RequestHandler handler{game};
+        // 5. HTTP обработчик с поддержкой статики
+        http_handler::RequestHandler handler{game, static_root};
 
-        // 5. Запуск HTTP-сервера
+        // 6. Запуск HTTP-сервера
         const auto address = net::ip::make_address("0.0.0.0");
         constexpr net::ip::port_type port = 8080;
         http_server::ServeHttp(ioc, {address, port}, [&handler](auto&& req, auto&& send) {
@@ -65,8 +69,9 @@ int main(int argc, const char* argv[]) {
 
         std::cout << "Server has started..."sv << std::endl;
 
-        // 6. Запуск io_context
+        // 7. Запуск io_context
         RunWorkers(num_threads, [&ioc] { ioc.run(); });
+
     } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
         return EXIT_FAILURE;
